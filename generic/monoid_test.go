e38bc70f53571f4feab17e118da2c231319816f9
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-11-16 19:44:47 +0200

    refactor monoid testing with structural composition

--- generic/monoid_test.go
+++ generic/monoid_test.go
@@ -2,6 +2,7 @@ package generic_test
 
 import (
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/fogfish/golem/generic"
@@ -9,33 +10,60 @@ import (
 
 //
 // example of monoid implementation
-type Int int
-
-func (v Int) Empty() generic.Monoid {
-	return Int(0)
+type MSeq struct {
+	value []int
 }
 
-func (v Int) Combine(x interface{}) generic.Monoid {
-	return v + x.(Int)
+func New(x int) *MSeq {
+	return &MSeq{[]int{x}}
+}
+
+func (seq *MSeq) Empty() generic.Monoid {
+	return &MSeq{}
+}
+
+func (seq *MSeq) Combine(x interface{}) generic.Monoid {
+	switch v := x.(type) {
+	case int:
+		seq.value = append(seq.value, v)
+	case *MSeq:
+		seq.value = append(seq.value, v.value...)
+	}
+	return seq
 }
 
 //
 // generic algorithm
-type seq struct {
-	x []Int
+type String struct {
+	value []string
 }
 
-func (s *seq) fold(f func(Int) Int, m generic.Monoid) generic.Monoid {
+func (seq *String) Map(m generic.Monoid, f func(string) int) generic.Monoid {
 	y := m.Empty()
-	for _, x := range s.x {
+	for _, x := range seq.value {
 		y = y.Combine(f(x))
 	}
 	return y
 }
 
+//
+// global constants
+var result *MSeq
+var sequence String = String{[]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}}
+
+func atoi(str string) int {
+	int, _ := strconv.Atoi(str)
+	return int
+}
+
+//
+// unit tests
 func TestIdentity(t *testing.T) {
-	x := Int(1).Combine(Int(1).Empty())
-	y := Int(1).Empty().Combine(Int(1))
+	a := New(1)
+	b := New(1)
+
+	x := a.Combine(a.Empty())
+	y := b.Empty().Combine(b)
 
 	if !reflect.DeepEqual(x, y) {
 		t.Fatalf("monoid violates identity %v != %v\n", x, y)
@@ -43,43 +71,42 @@ func TestIdentity(t *testing.T) {
 }
 
 func TestAssociativity(t *testing.T) {
-	x := Int(1).Combine(Int(2)).Combine(Int(3))
-	y := Int(1).Combine(Int(2).Combine(Int(3)))
+	x := New(1).Combine(New(2)).Combine(New(3))
+	y := New(1).Combine(New(2).Combine(New(3)))
 
 	if !reflect.DeepEqual(x, y) {
 		t.Fatalf("monoid violates associativity %v != %v\n", x, y)
 	}
 }
 
-func TestFold(t *testing.T) {
-	s := seq{[]Int{0, 1, 2, 3, 4, 5}}
-	y := s.fold(func(x Int) Int { return x }, Int(0))
-	if !reflect.DeepEqual(Int(15), y) {
-		t.Fatalf("failed to fold with monoid sum( %v ) != %v\n", s, y)
+func TestMap(t *testing.T) {
+	expect := &MSeq{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}
+	value := sequence.Map(New(0), atoi)
+
+	if !reflect.DeepEqual(expect, value) {
+		t.Fatalf("failed to map sequence %v != %v\n", expect, value)
 	}
 }
 
-var result Int
-
+//
+// benchmarks
 func BenchmarkMonoid(b *testing.B) {
-	var m Int
-	x := seq{[]Int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}
+	var monoid *MSeq
 
 	for n := 0; n < b.N; n++ {
-		m = x.fold(func(x Int) Int { return x * 10 }, Int(0)).(Int)
+		monoid = sequence.Map(monoid, atoi).(*MSeq)
 	}
-	result = m
+	result = monoid
 }
 
 func BenchmarkForLoop(b *testing.B) {
-	var m Int
-	x := seq{[]Int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}
+	var seq *MSeq
 
 	for n := 0; n < b.N; n++ {
-		m = 0
-		for _, v := range x.x {
-			m = m + v*10
+		seq = &MSeq{}
+		for _, x := range sequence.value {
+			seq.value = append(seq.value, atoi(x))
 		}
 	}
-	result = m
+	result = seq
 }
