684c35d8a0d01cc7aee94531d2a8e0423abc4588
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2021-04-04 21:32:08 +0300

    improve description of monoid in Golang

--- pure/monoid.go
+++ pure/monoid.go
@@ -8,44 +8,43 @@
 
 package pure
 
-// Monoid implements an algebraic structure with a single associative
-// binary operation and an identity element.
-// See the post about monoid in Go:
-// https://github.com/fogfish/golem/blob/master/doc/monoid.md
-//
-// Monoid supports the generic implementation of strcutural transformations.
-// It is an algebraic structure https://en.wikipedia.org/wiki/Monoid
-// with identity element and associative binary function.
-//
-// Monoid facilitates the implementation of following transformations in
-// the absence of generics and type covariance.
-//
-//    def map[B](f: (A) => B): Seq[B]
-//
-// The library uses Monoid interface to implement map, fold, filter,
-// comprehension and others for complex data structures. Here is an
-// example
-//
-//
-//   type MSeq struct { value []int }
-//
-//   func (seq *MSeq) Mempty() pure.Monoid {
-//     return &MSeq{}
-//   }
-//
-//   func (seq *MSeq) Mappend(x pure.Monoid) pure.Monoid {
-//     seq.value = append(seq.value, x.(*MSeq).value...)
-//     return seq
-//   }
-//
+/*
+
+Monoid implements an algebraic structure with a single associative
+binary operation and an identity element. See the post about monoid in Go:
+https://github.com/fogfish/golem/blob/master/doc/monoid.md
+
+Monoid supports the generic implementation of strcutural transformations.
+It is an algebraic structure https://en.wikipedia.org/wiki/Monoid
+with identity element and associative binary function.
+
+Monoid facilitates the implementation of following transformations in
+the absence of generics and type covariance.
+
+   def map[B](f: (A) => B): Seq[B]
+
+Monoid interface is usable to implement map, fold, filter,
+comprehension and others for complex data structures. Here is an
+example
+
+  type MSeq struct { value []int }
+
+  func (seq MSeq) Mempty() pure.Monoid {
+    return MSeq{}
+  }
+
+  func (seq MSeq) Mappend(x pure.Monoid) pure.Monoid {
+    return MSeq{append(seq.value, x.(MSeq).value...)}
+  }
+*/
 type Monoid interface {
 	// Mempty returns a type value that hold the identity property for
 	// combine operation, means the following equalities hold for any choice of x.
-	//   t.Combine(t.Empty()) == t.Empty().Combine(t) == t
+	//   t.Mappend(t.Mempty()) == t.Mempty().Mappend(t) == t
 	Mempty() Monoid
 
 	// Mappend applies a side-effect to the structure by appending a given value.
 	// Combine must hold associative property
-	//   a.Combine(b).Combine(c) == a.Combine(b.Combine(c))
+	//   a.Mappend(b).Mappend(c) == a.Mappend(b.Mappend(c))
 	Mappend(x Monoid) Monoid
 }
--- pure/monoid_test.go
+++ pure/monoid_test.go
@@ -9,53 +9,23 @@
 package pure_test
 
 import (
-	"reflect"
-	"strconv"
 	"testing"
 
 	"github.com/fogfish/golem/pure"
 )
 
 //
-// example of monoid implementation
-type MSeq struct {
-	value []int
-}
+// container type
+type SeqA []int
 
-func New(x int) *MSeq {
-	return &MSeq{[]int{x}}
-}
-
-//
-// Monoid interface for *MSeq
-func (seq *MSeq) Mempty() pure.Monoid {
-	return &MSeq{}
-}
-
-func (seq *MSeq) Mappend(x pure.Monoid) pure.Monoid {
-	seq.value = append(seq.value, x.(*MSeq).value...)
-	return seq
-}
-
-// type safe associative binary function
-func (seq *MSeq) Append(x *MSeq) {
-	seq.value = append(seq.value, x.value...)
-}
-
-//
-// generic algorithm
-type String struct {
-	value []string
-}
-
-type FMap func(string) pure.Monoid
+type FMap func(int) pure.Monoid
 type Mapper func(FMap) pure.Monoid
 
-// Functor
-func (seq *String) Map(m pure.Monoid) Mapper {
+// Map with Monoid, instantiate mapper
+func (seq SeqA) Map(m pure.Monoid) Mapper {
 	return func(f FMap) pure.Monoid {
 		y := m.Mempty()
-		for _, x := range seq.value {
+		for _, x := range seq {
 			y = y.Mappend(f(x))
 		}
 		return y
@@ -63,132 +33,104 @@ func (seq *String) Map(m pure.Monoid) Mapper {
 }
 
 // Map with Monoid
-func (seq *String) MMap(m pure.Monoid, f FMap) pure.Monoid {
+func (seq SeqA) MMap(m pure.Monoid, f FMap) pure.Monoid {
 	y := m.Mempty()
-	for _, x := range seq.value {
+	for _, x := range seq {
 		y = y.Mappend(f(x))
 	}
 	return y
 }
 
 // Map with Closure
-func (seq *String) FMap(f func(string)) {
-	for _, x := range seq.value {
+func (seq SeqA) FMap(f func(int)) {
+	for _, x := range seq {
 		f(x)
 	}
 }
 
 //
-// String x Monoid
-type Product struct {
-	String
-	M pure.Monoid
+// accumulator type implements monoid abstraction
+type SeqB int
+
+func (seq SeqB) Mempty() pure.Monoid {
+	return SeqB(0)
 }
 
-func (p *Product) Map(f FMap) pure.Monoid {
-	y := p.M.Mempty()
-	for _, x := range p.String.value {
-		y = y.Mappend(f(x))
-	}
-	return y
+func (seq SeqB) Mappend(x pure.Monoid) pure.Monoid {
+	return seq + x.(SeqB)
 }
 
 //
 // global constants
-var result *MSeq
-var sequence String = String{[]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}}
+var (
+	sequence SeqA = SeqA{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	result   pure.Monoid
+)
 
-func atog(str string) pure.Monoid {
-	x, _ := strconv.Atoi(str)
-	return &MSeq{[]int{x}}
-}
-
-func atoi(str string) *MSeq {
-	x, _ := strconv.Atoi(str)
-	return &MSeq{[]int{x}}
+// convAtoB is mapper function, converts type A to B
+func convAtoB(s int) pure.Monoid {
+	return SeqB(s)
 }
 
 //
-// unit tests
-func TestIdentity(t *testing.T) {
-	a := New(1)
-	b := New(1)
-
-	x := a.Mappend(a.Mempty())
-	y := b.Mempty().Mappend(b)
-
-	if !reflect.DeepEqual(x, y) {
-		t.Fatalf("monoid violates identity %v != %v\n", x, y)
+// forEach make benchmark of forLoop comparable to FMap
+func forEach() SeqB {
+	seq := SeqB(0)
+	for _, x := range sequence {
+		seq = joinAtoB(seq, x)
 	}
+	return seq
 }
 
-func TestAssociativity(t *testing.T) {
-	x := New(1).Mappend(New(2)).Mappend(New(3))
-	y := New(1).Mappend(New(2).Mappend(New(3)))
-
-	if !reflect.DeepEqual(x, y) {
-		t.Fatalf("monoid violates associativity %v != %v\n", x, y)
-	}
-}
-
-func TestMap(t *testing.T) {
-	expect := &MSeq{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}
-	value := sequence.MMap(New(0), atog)
-
-	if !reflect.DeepEqual(expect, value) {
-		t.Fatalf("failed to map sequence %v != %v\n", expect, value)
-	}
+func joinAtoB(b SeqB, a int) SeqB {
+	return b + SeqB(a)
 }
 
 //
 // benchmarks
 func BenchmarkMonoid(b *testing.B) {
-	var monoid *MSeq
+	b.ReportAllocs()
+
+	var monoid pure.Monoid = SeqB(0)
 
 	for n := 0; n < b.N; n++ {
-		monoid = sequence.MMap(monoid, atog).(*MSeq)
+		monoid = sequence.MMap(monoid, convAtoB)
 	}
 	result = monoid
 }
 
 func BenchmarkMonoidHoF(b *testing.B) {
-	var monoid *MSeq
+	b.ReportAllocs()
+
+	// TODO: rename test to instantiation
+	var monoid pure.Monoid = SeqB(0)
 	mapper := sequence.Map(monoid)
 
 	for n := 0; n < b.N; n++ {
-		monoid = mapper(atog).(*MSeq)
-	}
-	result = monoid
-}
-
-func BenchmarkMonoidProduct(b *testing.B) {
-	var monoid *MSeq
-	product := &Product{sequence, monoid}
-
-	for n := 0; n < b.N; n++ {
-		monoid = product.Map(atog).(*MSeq)
+		monoid = mapper(convAtoB)
 	}
 	result = monoid
 }
 
 func BenchmarkForLoop(b *testing.B) {
-	var seq *MSeq
+	b.ReportAllocs()
+
+	var seq SeqB
 
 	for n := 0; n < b.N; n++ {
-		seq = &MSeq{}
-		for _, x := range sequence.value {
-			seq.value = append(seq.value, atoi(x).value...)
-		}
+		forEach()
 	}
 	result = seq
 }
 
 func BenchmarkClojure(b *testing.B) {
-	var seq *MSeq
+	b.ReportAllocs()
+
+	var seq SeqB
 
 	for n := 0; n < b.N; n++ {
-		seq = &MSeq{}
-		sequence.FMap(func(x string) { seq.Append(atoi(x)) })
+		seq = SeqB(0)
+		sequence.FMap(func(x int) { seq = joinAtoB(seq, x) })
 	}
 	result = seq
 }
