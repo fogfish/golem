025d032fcd6cb3a2699e95561cc8e64e85ab46bf
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-05-12 21:07:37 +0300

    fix file headers

--- optics/iso.go
+++ optics/iso.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2022 - 2023 Dmitry Kolesnikov
+// Copyright (C) 2022 - 2024 Dmitry Kolesnikov
 //
 // This file may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
