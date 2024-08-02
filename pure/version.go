7a01f6dae0615c450de1b2ad1e089f9d10eae51e
fogfish <dmkolesnikov@gmail.com>
2024-08-02 23:48:44 +0300

    Update CI/CD for automated tagging (#50)

---
+++ pure/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2022 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/golem
+//
+
+package pure
+
+const Version = "pure/v0.10.1"
---
+++ trait/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2022 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/golem
+//
+
+package trait
+
+const Version = "trait/v0.2.0"
